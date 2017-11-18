6ac80848f5f3bcc0410b0e67d5dce2afee7a653d
Brian Weber <brian.weber1337@gmail.com>
2017-11-17 19:27:08 -0700

    created test file to mess with mySQL

---
+++ index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Turn on debug mode.
+import cgitb
+cgitb.enable()
+
+# Print necessary headers.
+print("Content-Type: text/html")
+print()
+print("<h1>index.py works!</h1>")
+
