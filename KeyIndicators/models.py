e61441c53782946d92507d2d81dc552b80c41539
Brian Weber <brian.weber1337@gmail.com>
2017-12-26 07:03:12 -0500

    removed post_save hooks on Profile that were causing problems. Profile must be edited manually in django admin.

--- KeyIndicators/models.py
+++ KeyIndicators/models.py
@@ -77,12 +77,12 @@ class Profile(models.Model):
     def __str__(self):
         return "username: " + self.user.username + ", ward: " + self.ward.name
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+# 
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
