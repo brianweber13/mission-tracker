fe9a8fb3e76b72a7544130f2d99f3c98bf0f9b57
Brian Weber <brian.weber1337@gmail.com>
2018-02-17 03:15:54 -0500

    added investigator to admin site, created fieldsets for user

--- KeyIndicators/admin.py
+++ KeyIndicators/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile
+from .models import Profile, Investigator
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -14,8 +14,15 @@ class ProfileInline(admin.StackedInline):
 
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
+    fieldsets = [
+        (None,               {'fields': ['Username','Password']}),
+        ('Personal info',    {'fields': ['first_name']}),
+    ]
     inlines = (ProfileInline, )
 
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+# other registrations
+admin.site.register(Investigator)
