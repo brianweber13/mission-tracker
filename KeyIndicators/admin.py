c77e5746edc9382b892f90b57bfe6bfe643030ce
Brian Weber <brian.weber1337@gmail.com>
2018-03-02 00:15:30 -0500

    admin page shows more about user, allows more KeyIndicators items to be edited

--- KeyIndicators/admin.py
+++ KeyIndicators/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Profile, Investigator
+from .models import Profile, Investigator, Ward, Stake, Event
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -15,8 +15,10 @@ class ProfileInline(admin.StackedInline):
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
     fieldsets = [
-        (None,               {'fields': ['Username','Password']}),
-        ('Personal info',    {'fields': ['first_name']}),
+        (None,              {'fields': ['username','password']}),
+        # ('Personal info',    {'fields': ['first_name']}),
+        (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (('Groups'),        {'fields': ('groups',)}),
     ]
     inlines = (ProfileInline, )
 
@@ -26,3 +28,6 @@ admin.site.register(User, UserAdmin)
 
 # other registrations
 admin.site.register(Investigator)
+admin.site.register(Ward)
+admin.site.register(Stake)
+admin.site.register(Event)
