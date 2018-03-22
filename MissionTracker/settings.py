977a11d4ffffd43098e42e068f46510cb8bd5c62
Brian Weber <brian.weber1337@gmail.com>
2018-03-22 17:14:28 -0400

    moved templates to within app

--- MissionTracker/settings.py
+++ MissionTracker/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'MissionTracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
