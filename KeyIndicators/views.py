ed92a15e26259af7b17edbf403357ef04e1b7ea5
Brian Weber <brian.weber1337@gmail.com>
2018-03-22 20:16:32 -0400

    created separate home and login views

--- KeyIndicators/views.py
+++ KeyIndicators/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
+# from django.http.HttpRequest import user
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+def home(request):
+    if request.user.is_authenticated:
+        context = { 'subtitle' : 'Home' }
+        return render(request, 'KeyIndicators/base.html', context)
+    else:
+        return redirect('login')
 
 def login(request):
-    context = { 'subtitle' : 'login'}
-    return render(request, 'KeyIndicators/login.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        context = { 'subtitle' : 'Login' }
+        return render(request, 'KeyIndicators/login.html', context)
